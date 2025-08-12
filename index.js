da5dd7809c84ee924314d37a3b424176244f402b
ShreyashWebCoder <shreyashwebcoder@gmail.com>
2025-08-12 19:27:45 +0530

    add link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://socialix.netlify.app",
     credentials: true,
 }))
 app.use(express.json());
