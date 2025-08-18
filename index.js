cd08f8369fba5a4ce7e3582a9dcd66f475236d9b
ShreyashWebCoder <shreyashwebcoder@gmail.com>
2025-08-18 19:24:35 +0530

    changes

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    // origin: "https://socialix.netlify.app",
-    origin: "http://localhost:5173",
+    origin: "https://socialix.netlify.app",
+    // origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
